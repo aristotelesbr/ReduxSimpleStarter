3ce4c1caa30c1b1883d7efea4a7dd645fd0bf657
aristotelesbr2014 <aristotelesbr2014@gmail.com>
2017-02-11 14:18:57 -0200

    Installing and configuring the youtube API plugin and creating my first true compiler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBrpyoim-LV9qHBvOtfgSOgS4U8Lp0IyEQ'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Creating new component. This componnet should produce
+// some HTML
+// const App = function() {
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it 
+//  on the (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
